a0185a72a914dbbf1b0b614a6f3081172ff7875e
Mauro Ttito <maurottito@gmail.com>
2026-02-04 16:56:08 -0600

    add conftest.py

---
+++ web/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def pytest_configure(config):
+    """Register custom pytest markers"""
+    config.addinivalue_line(
+        "markers", "integration: mark test as integration test (requires real database)"
+    )
